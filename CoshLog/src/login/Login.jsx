5d97ad1a0d8b05e358f5d8d9d94e1a19917313a7
tteugyul <yZQGCY0oHy0WrijW6cL7HQiHu2BKtt@gmail.com>
2026-06-24 12:14:01 +0900

    로그인 회원가입 2차 작업 완료(크기 조정 및 버튼 radius 및 색깔 블랙&화이트 변경)

--- CoshLog/src/login/Login.jsx
+++ CoshLog/src/login/Login.jsx
@@ -1,29 +1,39 @@
+import Register from '../register/Register';
 import './Login.css';
+import { useNavigate } from 'react-router';
 function Login() {
+    let navigate = useNavigate();
     return (
-        <div className='container'>
-            <form>
-                <div className='form'>
-                    <p className='loginTitle'>로그인</p>
-                    <div className='line'></div>
+        <div className='login_container'>
+            <form className='login_form' onClick={(event) => {
+                event.preventDefault();
+            }}>
+                <div className='login_box'>
+                    <p className='login_title'>로그인</p>
+                    <div className='login_line'></div>
                     <input className='login_inputID' type='text' placeholder="아이디 입력"></input>
                     <input className='login_inputPW' type='password' placeholder="비밀번호 입력"></input>
-                    <button className='btn_login'>로그인 버튼</button>
+                    <button className='login_btn' onClick={(event) => {
+                        alert('로그인이 완료되었습니다.');
+                    }}>로그인 버튼</button>
                     <p>OR</p>
-                    <button className='btn_login_naver'>
-                        <img style={{ width: '20%' }} src='https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyNjA2MTRfMjk2%2FMDAxNzgxMzcwODc3Nzgw.7MoG5UIDbDH4M0qAlU-UZhSyIxlzBm9hZi8MllM0oigg.fakJf3dwYPbsdG-c6mV_pt0d-leS0w5bKAnJn1rC2w8g.GIF%2F1.gif&type=sc960_832_gif' />
+                    <button className='login_btn_naver'>
+                        <img className='login_img_social' src='https://i.namu.wiki/i/p_1IEyQ8rYenO9YgAFp_LHIAW46kn6DXT0VKmZ_jKNijvYth9DieYZuJX_E_H_4GkCER_sVKhMqSyQYoW94JKA.svg' />
                         네이버 로그인
                     </button>
-                    <button className='btn_login_google'>
-                        <img style={{ width: '20%' }} src='https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyNjA2MTRfMjk2%2FMDAxNzgxMzcwODc3Nzgw.7MoG5UIDbDH4M0qAlU-UZhSyIxlzBm9hZi8MllM0oigg.fakJf3dwYPbsdG-c6mV_pt0d-leS0w5bKAnJn1rC2w8g.GIF%2F1.gif&type=sc960_832_gif' />
+                    <button className='login_btn_google'>
+                        <img className='login_img_social' src='https://img.icons8.com/matisse/1200/google.jpg' />
                         구글 로그인
                     </button>
-                    <button className='btn_findID'>아이디 찾기</button>
-                    <button className='btn_reg'>회원가입</button>
+                    <button className='login_btn_findID' onClick={() => {
+                        navigate('/findID')
+                    }}>아이디 찾기</button>
+                    <button className='login_btn_reg' onClick={() => {
+                        navigate('/register')
+                    }}>회원가입</button>
                 </div>
             </form>
         </div>
     );
 }
-
 export default Login;
--- CoshLog/src/register/Register.jsx
+++ CoshLog/src/register/Register.jsx
@@ -1,27 +1,33 @@
 import './Register.css';
+import { useNavigate } from 'react-router';
 function Register() {
     let str1 = '이름 입력';
     let str2 = "닉네임 입력";
     let str3 = '이메일 입력';
     let str4 = '아이디 입력 (소문자+숫자 6~12자 입력)';
     let str5 = '비밀번호 입력 (대소문자+숫자+특수기호 8자 이상 입력)';
+    let navigate = useNavigate();
     return (
-        <div className='container'>
-            <form>
-                <div className='form'>
-                    <div className='form'>
-                        <p className='loginTitle'>회원가입</p>
-                        <div className='line'></div>
-                        <span className='textColor'>이름</span><input className='inputName' type='text' placeholder={str1}></input><br></br>
-                        <span className='textColor'>닉네임</span><input className='inputNick' type='text' placeholder={str2}></input><br></br>
-                        <span className='textColor'>이메일</span><input className='inputNick' type='text' placeholder={str3}></input><br></br>
-                        <span className='textColor'>아이디</span><input className='inputID' type='text' placeholder={str4}></input><br></br>
-                        <span className='textColor'>비밀번호</span><input className='inputPW' type='password' placeholder={str5}></input><br></br>
-                        <input className='check_reg' type='checkbox'></input><span className='textColor'>서비스 이용약관 동의</span><br></br>
-                        <input className='check_reg' type='checkbox'></input><span className='textColor'>개인정보 처리방침 동의</span><br></br>
-                        <button className='btn_goLog'>로그인으로 돌아가기</button>
-                        <button className='btn_regCom'>가입하기</button>
-                    </div>
+        <div className='reg_container'>
+            <form className='reg_form' onClick={(event) => {
+                event.preventDefault();
+            }}>
+                <div className='reg_box'>
+                    <p className='reg_title'>회원가입</p>
+                    <div className='reg_line'></div>
+                    <span className='reg_textColor'>이름</span><input className='reg_inputName' type='text' placeholder={str1}></input><br></br>
+                    <span className='reg_textColor'>닉네임</span><input className='reg_inputNick' type='text' placeholder={str2}></input><br></br>
+                    <span className='reg_textColor'>이메일</span><input className='reg_inputEmail' type='text' placeholder={str3}></input><br></br>
+                    <span className='reg_textColor'>아이디</span><input className='reg_inputID' type='text' placeholder={str4}></input><br></br>
+                    <span className='reg_textColor'>비밀번호</span><input className='reg_inputPW' type='password' placeholder={str5}></input><br></br>
+                    <input className='reg_check_reg1' type='checkbox'></input><span className='reg_textColor'>서비스 이용약관 동의</span><br></br>
+                    <input className='reg_check_reg2' type='checkbox'></input><span className='reg_textColor'>개인정보 처리방침 동의</span><br></br>
+                    <button className='reg_btn_goLog' onClick={() => {
+                        navigate('/login');
+                    }}>로그인으로 돌아가기</button>
+                    <button className='reg_btn_regCom' onClick={() => {
+                        alert('가입이 완료되었습니다.');
+                    }}>가입하기</button>
                 </div>
             </form >
         </div >
