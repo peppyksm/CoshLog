eec3b0e8ec8fafe19e8429798e3ac7fb289e7987
tteugyul <yZQGCY0oHy0WrijW6cL7HQiHu2BKtt@gmail.com>
2026-06-25 10:42:44 +0900

    마이페이지 1차 작업 완료 / ui 개선 다수 필요

--- CoshLog/src/App.jsx
+++ CoshLog/src/App.jsx
@@ -3,15 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import Login from './login/Login'
-import Register from './register/Register'
+import { BrowserRouter } from "react-router";
+import Login from './login/Login';
+import Register from './register/Register';
+import MyPage from './mypage/MyPage';
 
 function App() {
 
   return (
     <>
-      <Login/>
-      {/* <Register /> */}
+      <BrowserRouter>
+        {/* <Login /> */}
+        {/* <Register /> */}
+        <MyPage />
+      </BrowserRouter>
     </>
   )
 }
