316e5f0c99954d0af0ac3e06d62e979e8f885c85
Seung-min <peppyksm@gmail.com>
2026-06-30 12:15:03 +0900

    비공개 기능 구현

--- CoshLog/src/pages/compnent/Post.jsx
+++ CoshLog/src/pages/compnent/Post.jsx
@@ -18,11 +18,17 @@ function Post({ ctg1, ctg2 }) {
                 ctgPosts.map((post) => (
                     <div className="postItem" key={post.id}>
                         <span style={{ marginLeft: "26%" }}>{post.ctg3}</span>
-                        <span style={{cursor : "pointer"}} onClick={() => {
-                            navigate(`/viewpost/${post.id}`);
+                        <span style={{ cursor: "pointer" }} onClick={() => {
+                            if (post.isPrivate == true) {
+                                alert("비공개 글입니다.")
+
+                            } else {
+                                navigate(`/viewpost/${post.id}`);
+                            }
+
                         }}>{post.title || "제목 없음"}</span>
                         {post.nickName == null ? (<span>게스트</span>) : (<span>{post.nickName}</span>)}
-                        
+
                         <span>
                             {post.time ? new Date(post.time).toLocaleDateString() : ""}
                         </span>
